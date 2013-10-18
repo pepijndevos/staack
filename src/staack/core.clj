ec492068851ff8d64ff2abb16da6972ea896a980
Pepijn de Vos <pepijndevos@gmail.com>
2013-10-18 10:24:01 +0200

    move common stuff to core

---
+++ src/staack/core.clj
@@ -0,0 +1,17 @@
+(ns staack.core)
+
+(def queue (clojure.lang.PersistentQueue/EMPTY))
+(def stack ())
+; ????
+
+(defn thrush [init & fns]
+  (reduce #(%2 %1) init fns))
+
+(defn lit [v]
+  #(conj % v))
+
+(defn block [& words]
+  #(apply trush % words))
+
+(defmacro defblock [name & words]
+  `(def ~name (block ~@words)))
