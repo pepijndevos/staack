24939d16a4d1eeda0e193d4d4489784213d7b91f
Pepijn de Vos <pepijndevos@gmail.com>
2013-10-18 10:15:22 +0200

    remove parallel code

--- src/staack/dataflow.clj
+++ src/staack/dataflow.clj
@@ -1,16 +1,11 @@
-(ns staack.parallel
-  (:require [clojure.core.reducers :as r]))
-
-(defn safe-print [& stuff]
-  (locking *out*
-    (apply println (.getId (Thread/currentThread)) stuff)))
+(ns staack.parallel)
 
 (deftype NegStack [popfn stack]
   clojure.lang.ISeq
   (first [_] nil)
   (more [_] ())
   (next [_] nil)
-  (cons [_ o] (safe-print "added" o) (popfn o stack))
+  (cons [_ o] (popfn o stack))
   clojure.lang.Seqable
   (seq [_] nil)
   clojure.lang.Counted
@@ -24,36 +19,10 @@
     (cont (first stack) (next stack))
     (NegStack. cont stack)))
 
-(defn ^:private conjcat [x y]
-  (let [s (seq x)]
-    (if s
-      (conj (conjcat (rest s) y) (first s))
-      y)))
-
-(defn ^:private consume [x y]
-  (fn [f stack]
-    (let [nx (conj x f)]
-      (if (negative-stack? nx)
-        (NegStack. (consume nx y) stack)
-        (conjcat nx y)))))
-
-(defn mconcat
-  ([] (safe-print "init") ())
-  ([base add]
-    (safe-print "combine" (count base) base (count add) add)
-    (if (negative-stack? base)
-      (if (negative-stack? add)
-        (NegStack. (consume base add) ())
-        (recur (conj base (first add)) (rest add)))
-      (conjcat base add))))
-
 (defn concall [stack f n]
   (if (> n 0)
     (mpeek stack #(concall %2 (partial f %1) (dec n)))
     (conj stack (f))))
 
 (defn thrush [fns]
-  (reduce (fn [a b] (safe-print (count a) a) (b a)) () fns))
-
-(defn pthrush [fns]
-  (r/fold 20 mconcat (fn [a b] (safe-print (count a) a) (b a)) fns))
+  (reduce (fn [a b] (b a)) () fns))
